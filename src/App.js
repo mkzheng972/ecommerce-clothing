55010243d8cd247fab4a7932be8339a889d6e6f6
mkzheng972 <markzheng27@gmail.com>
2020-06-26 21:54:29 -0400

    added collection-preview and collection-item and shoppage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
-import HomePage from './pages/homepage.component.jsx';
+import HomePage from './pages/homepage/homepage.component.jsx';
+import ShopPage from './pages/shoppage/shoppage.component.jsx';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage}></Route>
+        <Route path='/shop' component={ShopPage}></Route>
+      </Switch>
     </div>
   );
 }
